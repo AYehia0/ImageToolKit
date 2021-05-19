3bf9f26644da0899af3ba9e5e30231d249fa9831
AYehia0 <ahmedyehia611@gmail.com>
2021-05-19 21:23:21 +0200

    getting input colors as radio button

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 var sourceImg = document.getElementById("src-img");
 var imgUplaod = document.getElementById("fileUpload")
 
+
+// get input color 
+function getInputColor() {
+    var colors = document.getElementsByName("color")
+    
+    // color selector var 
+    var colorHolder = ""
+
+    for(let i=0; i<colors.length; i++) {
+        if (colors[i].checked) {
+            colorHolder = colors[i].value
+        }
+    }
+    return colorHolder
+}
+
 // onload handler for removing the welcome text after openCV is loading 
 window.onOpenCvReady = function () {
     document.getElementById("welcomeMsg").remove()
@@ -11,14 +27,30 @@ imgUplaod.onchange = function () {
 }
 //display the other modified img
 sourceImg.onload = function () {
+    //getting user choice of color
+    var colorChoice = getInputColor()
+
     //loading the image
-    let sourceMat = cv.imread(sourceImg)
-    let destMat = sourceMat.clone()
 
-    cv.cvtColor(sourceMat, destMat, cv.COLOR_RGBA2GRAY)
-    cv.imshow("edited-img", destMat)
+    if (colorChoice === "defaultColor"){
+        //display the color with no effects 
+        let sourceMat = cv.imread(sourceImg)
+        cv.imshow("edited-img", sourceMat)
+        sourceMat.delete()
 
-    destMat.delete()
-    sourceMat.delete()
+    }else{
+
+        let sourceMat = cv.imread(sourceImg)
+        let destMat = sourceMat.clone()
+        
+        cv.cvtColor(sourceMat, destMat, cv.COLOR_RGBA2GRAY)
+        cv.imshow("edited-img", destMat)
+        
+        // deleting 
+        destMat.delete()
+        sourceMat.delete()
+
+    }
+
 }
 
