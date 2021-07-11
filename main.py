6487c644093dae4ebbecaf2bf9e5a6843e5541f0
AYehia0 <ahmedyehia611@gmail.com>
2021-07-11 17:17:41 +0200

    splitted to main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from image_ui import *
+import opencv_back
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
